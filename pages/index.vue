ce09c148a3eda1a97a37ba7491b97e859ec8a683
Sofyan Waldy <waldy@tirto.id>
2019-09-21 00:42:06 +0700

    update homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,38 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
-      <div class="text-center"></div>
+      <div class="text-center">
+        <img src="/img/corpu.png" width="200" alt="logo" />
+      </div>
+      <v-content class="text-left">
+        FETA SCHOLARSHIP FETA Scholarship dilaksanakan dalam rangka memenuhi
+        Rencana Pengembangan SDM Kementerian Keuangan sebagaimana tertuang dalam
+        Keputusan Menteri Keuangan Nomor 51/KMK.01/2012 tentang Rencana Program
+        Pengembangan SDM Kementerian Keuangan, yang salah satunya adalah
+        kebutuhan pegawai dengan latar belakang pendidikan pascasarjana (S2/S3).
+        Tujuan diselenggarakannya program ini adalah meningkatkan kapasitas SDM
+        organisasi yang berkaitan dengan pelaksanaan tugas pokok dan fungsi,
+        menciptakan critical mass mengenai pelaksanaan tata kelola pemerintahan
+        yang baik melalui transformasi kelembagaan, dan menantang cara pandang
+        strategis atas proses transformasi kelembagaan, mengubah pola piker
+        dalam menjalankan tugas dan fungsi serta memperkenalkan cara berpikir
+        lateral dalam menjalankan organisasi Kementerian Keuangan.<br />
+        Dasar Hukum Tugas Belajar : 18/ PMK.01/ 2009 <br />Pilihan :
+        <a
+          href="http://www.jdih.kemenkeu.go.id/fullText/2009/18~PMK.01~2009per.htm"
+          target="_blank"
+          >http://www.jdih.kemenkeu.go.id/fullText/2009/18~PMK.01~2009per.htm
+        </a>
+        <br />
+        Download/Lihat
+        <a href="/img/pmk_18_tahun_2009.pdf" target="_blank">Lampiran </a>
+        <br />
+        <div class="text-center">
+          <img
+            src="/img/kemenkeu-Padma-Radya-Aktuaria-Konsultan-Aktuaria-Actuary-Consulting-In-Indonesia.png"
+          />
+        </div>
+      </v-content>
     </v-flex>
   </v-layout>
 </template>
